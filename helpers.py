501334fe5e24fad942c78a5930f1e924003a05c4
root <root@localhost.localdomain>
2021-03-13 16:25:29 +0000

    Removing docstrings

--- helpers.py
+++ helpers.py
@@ -8,17 +8,11 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def parse_yaml(file):   
     '''
-    Just parsing yaml and returing the native python object.
-    file(str): name of yaml file
-    '''
     with open(file) as file:
         result = yaml.load(file, Loader=yaml.FullLoader)
     return(result)
 
 def generate_cfg_from_template(template_file, data_dict): 
-    '''
-    Just generating a string with template using a data_dict
-    '''
     template = Template(open(template_file).read())  
     return(template.render(data_dict)) 
 
@@ -33,19 +27,13 @@ def prep_config(generated_config, api_key):
     return(to_push)
 
 def post_config(target, to_push):
-    '''
-    Posting data to the target
-    '''
     url = 'https://' + target + '/configure'
     r = requests.post(url, files=to_push, verify=False)
     return(r)
 
 def save_config(target, api_key):
-   '''
-   Just saving cofig you know...
-   '''
-   url = 'https://' + target + '/config-file'
-   data = '{"op": "save"}'
-   to_push = prep_config(data, api_key)
-   r = requests.post(url, files=to_push, verify=False)
-   return(r)
+    url = 'https://' + target + '/config-file'
+    data = '{"op": "save"}'
+    to_push = prep_config(data, api_key)
+    r = requests.post(url, files=to_push, verify=False)
+    return(r)
